efbd6eddeaf24e5b71f334ee5841dec3e7a4dc6a
KOBEBOSS <66833322+KOBEBOSS@users.noreply.github.com>
2020-06-13 21:02:54 +0300

    Add files via upload

---
+++ sayi/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace sayi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.rasgeleOlustur = new System.Windows.Forms.Button();
+            this.hesapla = new System.Windows.Forms.Button();
+            this.hedefSayi = new System.Windows.Forms.TextBox();
+            this.sayi1 = new System.Windows.Forms.TextBox();
+            this.sayi2 = new System.Windows.Forms.TextBox();
+            this.sayi3 = new System.Windows.Forms.TextBox();
+            this.sayi4 = new System.Windows.Forms.TextBox();
+            this.sayi5 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(166, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(255, 55);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sayı Ödevi";
+            // 
+            // rasgeleOlustur
+            // 
+            this.rasgeleOlustur.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.rasgeleOlustur.Location = new System.Drawing.Point(26, 222);
+            this.rasgeleOlustur.Name = "rasgeleOlustur";
+            this.rasgeleOlustur.Size = new System.Drawing.Size(259, 44);
+            this.rasgeleOlustur.TabIndex = 2;
+            this.rasgeleOlustur.Text = "Rastgele Sayılar Oluştur";
+            this.rasgeleOlustur.UseVisualStyleBackColor = true;
+            this.rasgeleOlustur.Click += new System.EventHandler(this.rasgeleOlustur_Click);
+            // 
+            // hesapla
+            // 
+            this.hesapla.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.hesapla.Location = new System.Drawing.Point(291, 222);
+            this.hesapla.Name = "hesapla";
+            this.hesapla.Size = new System.Drawing.Size(271, 44);
+            this.hesapla.TabIndex = 2;
+            this.hesapla.Text = "Hesapla";
+            this.hesapla.UseVisualStyleBackColor = true;
+            this.hesapla.Click += new System.EventHandler(this.hesapla_Click);
+            // 
+            // hedefSayi
+            // 
+            this.hedefSayi.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.hedefSayi.Location = new System.Drawing.Point(211, 105);
+            this.hedefSayi.MaxLength = 3;
+            this.hedefSayi.Name = "hedefSayi";
+            this.hedefSayi.Size = new System.Drawing.Size(166, 32);
+            this.hedefSayi.TabIndex = 3;
+            this.hedefSayi.Text = "Hedeflenen Sayı";
+            this.hedefSayi.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // sayi1
+            // 
+            this.sayi1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.sayi1.Location = new System.Drawing.Point(26, 161);
+            this.sayi1.MaxLength = 3;
+            this.sayi1.Name = "sayi1";
+            this.sayi1.Size = new System.Drawing.Size(98, 26);
+            this.sayi1.TabIndex = 3;
+            this.sayi1.Text = "Sayı 1";
+            this.sayi1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // sayi2
+            // 
+            this.sayi2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.sayi2.Location = new System.Drawing.Point(130, 161);
+            this.sayi2.MaxLength = 3;
+            this.sayi2.Name = "sayi2";
+            this.sayi2.Size = new System.Drawing.Size(98, 26);
+            this.sayi2.TabIndex = 3;
+            this.sayi2.Text = "Sayı 2";
+            this.sayi2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // sayi3
+            // 
+            this.sayi3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.sayi3.Location = new System.Drawing.Point(234, 161);
+            this.sayi3.MaxLength = 3;
+            this.sayi3.Name = "sayi3";
+            this.sayi3.Size = new System.Drawing.Size(98, 26);
+            this.sayi3.TabIndex = 3;
+            this.sayi3.Text = "Sayı 3";
+            this.sayi3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // sayi4
+            // 
+            this.sayi4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.sayi4.Location = new System.Drawing.Point(338, 161);
+            this.sayi4.MaxLength = 3;
+            this.sayi4.Name = "sayi4";
+            this.sayi4.Size = new System.Drawing.Size(98, 26);
+            this.sayi4.TabIndex = 3;
+            this.sayi4.Text = "Sayı 4";
+            this.sayi4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // sayi5
+            // 
+            this.sayi5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.sayi5.Location = new System.Drawing.Point(442, 161);
+            this.sayi5.MaxLength = 3;
+            this.sayi5.Name = "sayi5";
+            this.sayi5.Size = new System.Drawing.Size(120, 26);
+            this.sayi5.TabIndex = 3;
+            this.sayi5.Tag = "";
+            this.sayi5.Text = "Sayı 5(çift hane)";
+            this.sayi5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(597, 296);
+            this.Controls.Add(this.sayi5);
+            this.Controls.Add(this.sayi4);
+            this.Controls.Add(this.sayi3);
+            this.Controls.Add(this.sayi2);
+            this.Controls.Add(this.sayi1);
+            this.Controls.Add(this.hedefSayi);
+            this.Controls.Add(this.hesapla);
+            this.Controls.Add(this.rasgeleOlustur);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button rasgeleOlustur;
+        private System.Windows.Forms.Button hesapla;
+        private System.Windows.Forms.TextBox hedefSayi;
+        private System.Windows.Forms.TextBox sayi1;
+        private System.Windows.Forms.TextBox sayi2;
+        private System.Windows.Forms.TextBox sayi3;
+        private System.Windows.Forms.TextBox sayi4;
+        private System.Windows.Forms.TextBox sayi5;
+    }
+}
+
---
+++ sayi/Form1.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sayi
+{
+
+
+
+    class formPermut
+    {
+        public int[,] permutated = new int[721, 5];
+        public int index = 0;
+        public void swapTwoNumber(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+        public void prnPermut(int[] list, int k, int m)
+        {
+            int i;
+            if (k == m)
+            {
+                for (i = 0; i <= m; i++)
+                    Console.Write("{0}", list[i]);
+                Console.Write(" ");
+            }
+            else
+                for (i = k; i <= m; i++)
+                {
+                    swapTwoNumber(ref list[k], ref list[i]);
+                    prnPermut(list, k + 1, m);
+                    swapTwoNumber(ref list[k], ref list[i]);
+                }
+            
+            permutated[index, 0] = Convert.ToInt32(list[0]);
+            permutated[index, 1] = Convert.ToInt32(list[1]);
+            permutated[index, 2] = Convert.ToInt32(list[2]);
+            permutated[index, 3] = Convert.ToInt32(list[3]);
+            permutated[index, 4] = Convert.ToInt32(list[4]);
+
+            index++;
+
+        }
+    }
+
+    public partial class Form1 : Form
+    {
+        formPermut permutasyon = new formPermut();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int olusturulmus = 0;
+
+        private void rasgeleOlustur_Click(object sender, EventArgs e)
+        {
+
+            olusturulmus = 1;
+            Random rastgele = new Random();
+
+            int rastgeleTamSayiOlustur(int min, int maks, int ondalikCarpim){
+
+                return rastgele.Next(min, maks) * ondalikCarpim;
+            }
+
+            sayi1.Text = rastgeleTamSayiOlustur(1, 10, 1).ToString();
+            sayi2.Text = rastgeleTamSayiOlustur(1, 10, 1).ToString();
+            sayi3.Text = rastgeleTamSayiOlustur(1, 10, 1).ToString();
+            sayi4.Text = rastgeleTamSayiOlustur(1, 10, 1).ToString();
+            sayi5.Text = rastgeleTamSayiOlustur(1, 10, 10).ToString();
+            hedefSayi.Text = rastgeleTamSayiOlustur(100, 999, 1).ToString();
+        }
+
+        private void hesapla_Click(object sender, EventArgs e)
+        {
+
+            if (olusturulmus == 1)
+            {
+                int karsilastir(string dizi)
+                {
+                    string[] haystack = { "5", "6", "7", "8", "9", "0" };
+
+                    int check = 0;
+                    for (int u = 0; u < 4; u++)
+                    {
+                        for (int y = 0; y < 6; y++)
+                        {
+                            if (dizi[u].ToString() == haystack[y])
+                            {
+                                check = 1;
+                            }
+                        }
+                    }
+
+                    if (check == 1)
+                    {
+                        return 1;
+                    } else {
+                        return 0;
+                    }
+                }
+
+                int[] sayilar = new int[5];
+                sayilar[0] =Convert.ToInt32(sayi1.Text);
+                sayilar[1] =Convert.ToInt32(sayi2.Text);
+                sayilar[2] = Convert.ToInt32(sayi3.Text);
+                sayilar[3] = Convert.ToInt32(sayi4.Text);
+                sayilar[4] = Convert.ToInt32(sayi5.Text);
+                //MessageBox.Show(sayilar[0].ToString() + sayilar[1].ToString() + sayilar[2].ToString() + sayilar[3].ToString() + sayilar[4].ToString());
+
+
+                int enAzFark = 9999999;
+                string enAzFarkFoo = "3333";
+                int temp1 = 0;
+                int temp2 = 0;
+                int temp3 = 0;
+                int temp4 = 0;
+                int temp5 = 0;
+                permutasyon.prnPermut(sayilar, 0, 4);
+                permutasyon.index = 0;
+                for (int p = 0; p < 120; p++)
+                {
+                    sayilar[0] = permutasyon.permutated[p, 0];
+                    sayilar[1] = permutasyon.permutated[p, 1];
+                    sayilar[2] = permutasyon.permutated[p, 2];
+                    sayilar[3] = permutasyon.permutated[p, 3];
+                    sayilar[4] = permutasyon.permutated[p, 4];
+                    //MessageBox.Show(permutasyon.permutated[p, 0].ToString() + permutasyon.permutated[p, 1].ToString() + permutasyon.permutated[p, 2].ToString() + permutasyon.permutated[p, 3].ToString() + permutasyon.permutated[p, 4].ToString());
+                    //MessageBox.Show(sayilar[0].ToString() + sayilar[1].ToString() + sayilar[2].ToString() + sayilar[3].ToString() + sayilar[4].ToString());
+             
+
+                    for (int i = 1111; i <= 4444; i++)
+                {
+                    string foo = i.ToString();
+
+                    if (karsilastir(foo) == 0)
+                    {
+                        int bar = 0;
+
+                    
+
+                        for (int key = 0; key < (sayilar.Length); ++key)
+                        {
+                            int val = sayilar[key];
+                            if (key == 0)
+                            {
+                                bar = val;
+
+                            } else {
+
+                                if (foo[key - 1] == '1')
+                                {
+                                    bar += val;
+                                } else if (foo[key - 1] == '2')
+                                {
+                                    bar -= val;
+                                }
+                                else if (foo[key - 1] == '3')
+                                {
+                                    bar *= val;
+                                }
+                                else if (foo[key - 1] == '4')
+                                {
+                                    bar /= val;
+                                }
+                            }
+                        }
+
+                        if (Math.Abs(Convert.ToInt32(hedefSayi.Text) - bar) < enAzFark)
+                        {
+                            enAzFark = Math.Abs(Convert.ToInt32(hedefSayi.Text) - bar);
+                            enAzFarkFoo = foo;
+                                temp1 = sayilar[0];
+                                temp2 = sayilar[1];
+                                temp3 = sayilar[2];
+                                 temp4 = sayilar[3];
+                                 temp5 = sayilar[4];
+                            }
+                    }
+                }
+            }
+                
+                string isaretiYazdir(char sayisi)
+                {
+                    switch (sayisi)
+                    {
+                        case '1':
+                            return "+";
+                        case '2':
+                            return "-";
+                        case '3':
+                            return "*";
+                        case '4':
+                            return "/";
+                        default: return sayisi.ToString();
+                    }
+                }
+
+                int sonuc = Convert.ToInt32(hedefSayi.Text) + enAzFark;
+
+                string text = "+- " + enAzFark + " değeri kadar yaklaşıldı.\n";
+                text += temp1.ToString() + " " + isaretiYazdir(Convert.ToChar(enAzFarkFoo[0])) + " ";
+                text += temp2.ToString() + " " + isaretiYazdir(Convert.ToChar(enAzFarkFoo[1])) + " ";
+                text += temp3.ToString() + " " + isaretiYazdir(Convert.ToChar(enAzFarkFoo[2])) + " ";
+                text += temp4.ToString() + " " + isaretiYazdir(Convert.ToChar(enAzFarkFoo[3])) + " ";
+                text += temp5.ToString();
+                text += " = " + sonuc;
+
+                MessageBox.Show(text);
+               
+
+            }
+            else{
+
+                MessageBox.Show("Önce sayı oluşturmalısınız!");
+            }
+        }
+    }
+}
